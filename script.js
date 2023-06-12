49cd78979091fa749bc1d74e81c7719be1c76bf0
Serhii <ua.shuhlya@gmail.com>
2023-06-12 13:48:43 +0100

    Practice 2

--- script.js
+++ script.js
@@ -13,9 +13,27 @@ let personalMovieDB = {
 };
 
 
-for (let index = 0; index < 2; index++) {
-	let moviesName = prompt('Один из последних просмотренных фильмов?', ''),
+for (let index = 0; index < personalMovieDB.count; index++) {
+	let moviesName = '',
+		moviesRating = '';
+
+	while (!moviesName || moviesName.length > 50){
+		moviesName = prompt('Один из последних просмотренных фильмов?', '');
+	}
+	while (!moviesRating){
 		moviesRating = +prompt('На сколько оцените его?', '');
+	}
+
+
+	if (personalMovieDB.count < 10){
+		alert('Просмотрено довольно мало фильмов');
+	}else if (10 <= personalMovieDB.count <= 30){
+		alert('Вы классический зритель');
+	}else if (personalMovieDB.count > 30){
+		alert('Вы киноман');
+	}else{
+		alert('Произошла ошибка');
+	}
 
 	personalMovieDB.movies[moviesName] = moviesRating;
 	
