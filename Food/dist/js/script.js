f84c45e470d926b65584b0533f1f932c8116e43c
Serhii <ua.shuhlya@gmail.com>
2023-06-17 22:23:36 +0100

    food project 1 home work

---
+++ Food/dist/js/script.js
@@ -0,0 +1,42 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+document.addEventListener('DOMContentLoaded', () => {
+  const tabsContent = document.querySelectorAll('.tabcontent'),
+    tabs = document.querySelectorAll('.tabheader__item'),
+    tabsParent = document.querySelector('.tabheader__items');
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+  function showTabContent() {
+    let i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+  hideTabContent();
+  showTabContent();
+  tabsParent.addEventListener('click', event => {
+    event.preventDefault();
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
